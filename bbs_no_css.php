7273a0acdb588cf4eb051f04e4a4917d5f866aa3
森定遼 <seventh.mark.1227.zng@gmail.com>
2016-10-12 19:48:45 -0700

    2日目アップデート

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -17,6 +17,37 @@
   $stmt->execute();
   }
 
+
+//SQL文作成(SELECT文)
+ $sql = 'SELECT * FROM `posts`ORDER BY `created` DESC';
+
+//SELECT文実行
+ $stmt = $dbh->prepare($sql);
+ $stmt -> execute();
+
+
+//変数にDBから取得したデータを格納
+
+//格納する変数の初期化
+$posts = array();
+
+//繰り返し文でデータの取得
+while(1){
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($rec == false){
+      //データを最後まで取得したので終了
+      break;
+
+
+    }
+    //取得したデータを配列に格納しておく
+    $posts[] = $rec;
+
+}
+
+
+
+// DBの切断
   $dbh = null;
 ?>
 
@@ -33,6 +64,34 @@
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
+    <!-- <ul>
+     <li><?php //echo $posts[0]['nickname']; ?><?php
+      //echo $post[0]['comment'];
+     ?>
+     comment 2016-10-13</li>
+     <li>testname 一言つぶやき　2016-10-12</li>
+     <li>テスト太郎　コメント　2016-10-11</li>
+    </ul> -->
+<ul>
+  <?php
+     foreach ($posts as $post_each) {
+       echo '<li>';
+       echo $post_each['nickname'].' ';
+       echo $post_each['comment'].' ';
+
+       //一旦日付型に変換
+       $created = strtotime($post_each['created']);
+
+      //書式を変換
+      $created = date('Y/m/d',$created);
+
+       //echo $post_each['created'].' ';
+      echo $created;
+      echo '</li>';
+      }
+  ?>
+</ul>
+
 
 </body>
 </html>
